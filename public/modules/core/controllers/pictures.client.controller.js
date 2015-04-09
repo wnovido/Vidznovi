12d76cc5f2dc98ea9f2dfcddd066721375b7f9b4
wilso_000 <wilson_novido@yahoo.com>
2015-04-09 15:05:20 -0400

    Changes request from client

--- public/modules/core/controllers/pictures.client.controller.js
+++ public/modules/core/controllers/pictures.client.controller.js
@@ -3,10 +3,22 @@
  */
 'use strict';
 
-angular.module('core').controller('PictureController', ['$scope',
+angular.module('core')
+
+.controller('PictureController', ['$scope',
     function($scope) {
         // Aboutme controller logic
         // ...
-        $scope.text = 'Edna';
+        $scope.pictures = [{
+            filepath: "modules/albums/places/1265282_10202300476172512_195772845_o.jpg"
+        },
+            {
+                filepath: "modules/albums/places/1265390_10202300464212213_950848158_o.jpg"
+            },
+            {
+                filepath: "modules/albums/places/1265744_10202300448531821_1722655269_o.jpg"
+            }
+
+        ];
     }
 ]);
