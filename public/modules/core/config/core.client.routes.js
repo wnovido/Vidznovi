85b9c2345e743a9d9b63ee02e62743e55b10f018
wilso_000 <wilson_novido@yahoo.com>
2015-04-09 12:07:11 -0400

    Render pictures

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -11,6 +11,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
+		state('pictures', {
+			url: '/pictures',
+			templateUrl: 'modules/core/views/pictures.client.view.html'
 		});
 	}
 ]);
---
+++ public/modules/core/controllers/pictures.client.controller.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wilso_000 on 4/9/2015.
+ */
+'use strict';
+
+angular.module('core').controller('PictureController', ['$scope',
+    function($scope) {
+        // Aboutme controller logic
+        // ...
+        $scope.text = 'Edna';
+    }
+]);
