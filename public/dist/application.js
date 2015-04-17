c2165ccb4250154eb9863e7678d3d94d2af25d0c
wilso_000 <wilson_novido@yahoo.com>
2015-04-17 11:50:00 -0400

    Make the path dynamic base from the tables

--- public/dist/application.js
+++ public/dist/application.js
@@ -441,23 +441,8 @@ angular.module('core').controller('HomeController', ['$scope', '$animate', 'Auth
 angular.module('core').controller('PictureRenderController', ['$scope','$stateParams', 'Pictures', 'Albums', 'Albumgroups',
     function($scope, $stateParams, Pictures, Albums, Albumgroups) {
 
-        //$scope.pathname = 'modules/core/img/slider/';
-
         $scope.pictures = Pictures.query();
         $scope.albumFilter = $stateParams.albumId;
-
-        $scope.album = $stateParams.album;
-        $scope.albumId = $stateParams.albumId;
-        //$scope.albumgroupId = $stateParams.albumgroupId;
-        console.log($stateParams.albumId);
-        console.log($stateParams.albumName);
-        console.log($stateParams.albumgroupName);
-        //$scope.album = Albums.get({
-        //    albumId: $stateParams.album._id
-        //});
-
-
-        //$scope.albumgroup = Albumgroups.get(albumgroupId);
         $scope.pathname = 'photoalbums/' + $stateParams.albumgroupName + '/' + $stateParams.albumName + '/';
         console.log($scope.pathname);
 
