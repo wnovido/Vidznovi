3b12b9b4224b520fa357ddeed4bf3dece84c1ac5
wilso_000 <wilson_novido@yahoo.com>
2015-04-17 23:01:49 -0400

    Add more pictures

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,29 +1,41 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', '$animate', 'Authentication', 'Albums', 'Albumgroups',
-	function($scope, $animate, Authentication, Albums, Albumgroups) {
+angular.module('core').controller('HomeController', ['$scope', '$animate', 'Authentication', 'Albums', 'Albumgroups', 'Pictures',
+	function($scope, $animate, Authentication, Albums, Albumgroups, Pictures) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 		$animate.enabled(false);
 
 		$scope.myInterval = 5000;
+
 		var slides = $scope.slides = [];
-		$scope.addSlide = function() {
-			var newWidth = slides.length + 1;
-			slides.push({
-				image: 'modules/core/img/slider/' + newWidth + '.jpg',
-				text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-				['Gorgeous', 'Cutify', 'Felines', 'Cutes'][slides.length % 4]
-			});
-		};
-		for (var i=0; i<10; i++) {
-			$scope.addSlide();
-		}
+		//$scope.addSlide = function() {
+		//	var newWidth = slides.length + 1;
+		//	slides.push({
+		//		image: 'modules/core/img/slider/' + newWidth + '.jpg',
+		//		text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+		//		['Gorgeous', 'Cutify', 'Felines', 'Cutes'][slides.length % 4]
+		//	});
+		//};
+		//for (var i=0; i<10; i++) {
+		//	$scope.addSlide();
+		//}
+
+		slides.push({
+					image: 'modules/core/img/photoalbums/Fine Arts/B&W/10452979_10207046983472228_1013604151802666707_o.jpg'
+				});
+		slides.push({
+			image: 'modules/core/img/photoalbums/Fine Arts/B&W/11079581_10207046984472253_5326214675745713581_o.jpg'
+		});
 
 		$scope.tabs = Albumgroups.query();
 		$scope.albums = Albums.query();
+		$scope.slides = Pictures.query();
 
+		//this.forEach(function(item){
+		//	console.log(1);
+		//});
 
 	}
 ]);
--- public/modules/core/controllers/pictures.render.client.controller.js
+++ public/modules/core/controllers/pictures.render.client.controller.js
@@ -10,10 +10,5 @@ angular.module('core').controller('PictureRenderController', ['$scope','$statePa
         $scope.albumFilter = $stateParams.albumId;
         $scope.pathname = 'modules/core/img/photoalbums/' + $stateParams.albumgroupName + '/' + $stateParams.albumName + '/';
 
-        console.log($stateParams.albumId);
-        console.log($stateParams.albumgroupName);
-        console.log($stateParams.albumName);
-        console.log($scope.pathname);
-
     }
 ]);
